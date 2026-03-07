019c1f56a1241a9176c5e9ce5803c2bf6e31116e
pamelacarsonbri <pamelacarsonbri@gmail.com>
2026-03-07 04:03:49 +0000

    Add string practice file for week3

---
+++ week3-python/string_practice.py
@@ -0,0 +1,40 @@
+#Task 1
+first_name = "Pamela"
+second_name = "Carson"
+age = 20
+favorite_concept = "Error exception"
+
+print(first_name)
+print(second_name)
+print(age)
+print(favorite_concept)
+
+#Task 2
+Full_name = first_name + " " + second_name
+print(Full_name)
+
+Full_name = f"{first_name} {second_name}"
+print(Full_name)
+
+
+#Task 3
+print(Full_name.upper())
+
+print(Full_name.lower())
+
+print(Full_name.count('a'))
+
+print(Full_name[6])
+print(Full_name.replace("Pamela","Coder"))
+
+#Task 4
+print(f"Hi, I am {first_name}. I am {age} years old and my favourite concept so far is {favorite_concept}.")
+
+#Task 5
+print(Full_name[0])
+ 
+print(Full_name[-1])
+
+print(Full_name[0:7])
+
+
