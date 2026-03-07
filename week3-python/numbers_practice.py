f41d443295486a6f235bc4d14850f536fdb0b51a
pamelacarsonbri <pamelacarsonbri@gmail.com>
2026-03-07 04:01:26 +0000

    Add numbers practice file for week3

---
+++ week3-python/numbers_practice.py
@@ -0,0 +1,44 @@
+#Task 1
+int1 = 25
+int2 = 5
+float1 = 3.14
+float2 = 7.5
+
+
+print(int1 + int2)   # Addition: adds int1 and int2
+print(int1 - int2)   # Subtraction: subtracts int2 from int1
+print(int1 * int2)   # Multiplication: multiplies int1 by int2
+print(int1 / int2)   # Division: divides int1 by int2 (returns float)
+print(int1 // int2)  # Floor division: divides and rounds DOWN to nearest whole number
+print(int1 % int2)   # Modulo: returns the remainder after division
+print(int1 ** int2)  # Exponentiation: raises int1 to the power of int2
+
+
+length = 10
+width = 4
+
+area = length * width
+perimeter = 2 * (length + width)
+
+
+print(f"A rectangle with length {length} and width {width} has an area of {area} and a perimeter of {perimeter}.")
+
+
+x = 7
+x = float(x)
+print(x)
+
+y = 2.5
+y = int(y)
+print(y)
+
+a = 2.5
+b = "Accra"
+c = 9
+d = 7.9
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
